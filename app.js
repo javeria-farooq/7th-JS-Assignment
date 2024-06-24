a4368faae3a9a2685c1e8d79b30136a5e41ba2f5
javeria-farooq <gettingeducation6@gmail.com>
2024-06-24 23:32:08 +0500

    Initial commit

--- app.js
+++ app.js
@@ -1,48 +1,76 @@
-// 13
-// let text = "Result"
-// let a = 10
-// document.write(text + "<br>")
-// document.write("The value of a is: " + a + "<br>")
-// document.write("........................................................" + "<br>" + "<br>")
+/*increment and decrement */
 
-// res1 = ++a
-// document.write("The value of ++a is: " + res1 + "<br>")
-// document.write("Now the value of a is: " + a + "<br>" + "<br>")
+/*let num1 = 6
+let num2 = 3
+let res = num1++ + num1++ + ++num2 + num2++ + num1++
+            //6     +  7     +   4    +  4     +   8
+document.write(res, "<br>", "<br>")*/
 
-// res2 = a++
-// document.write("The value of a++ is: " + res2 + "<br>")
-// document.write("Now the value of a is: " + a + "<br>" + "<br>")
+/*let num1 = 6
+let num2 = 3
+let num3 = 10
+let res2 = --num1 - --num2 + num3++ + num3-- + num1
+            //5   -   2    +   10   +  11    +   5
+document.write(res2)*/
 
-// res3 = --a
-// document.write("The value of --a is: " + res3 + "<br>")
-// document.write("Now the value of a is: " + a + "<br>" + "<br>")
+/*let a = 4
+let b = 2
+let c = 10
+let e;
+let eq = a++ + ++a - --b + c++ + e++ + c++ + ++a + a
+        //4  +  6  -  1  +  10 +     +  11 +  7  + 7 = 44
+document.write(eq)*/
 
-// res4 = a--
-// document.write("The value of a-- is: " + res4 + "<br>")
-// document.write("Now the value of a is: " + a + "<br>" + "<br>")
+/*BODMAS in programming */
+/*let y = 10
+let u = 5
+let w = 2
+let res = y - w * u * (y + w)  + y
+          //10 - 2 * 5 * (10 + 2) + 10
+          //10 - 2 * 5 *     12   + 10
+          //10 - 10    *   12     + 10
+          //10 - 120              + 10  = -100
+document.write(res)*/
 
+/*// 13
+let text = "Result"
+let a = 10
+document.write(text + "<br>")
+document.write("The value of a is: " + a + "<br>")
+document.write("........................................................" + "<br>" + "<br>")
+res1 = ++a
+document.write("The value of ++a is: " + res1 + "<br>")
+document.write("Now the value of a is: " + a + "<br>" + "<br>")
+res2 = a++
+document.write("The value of a++ is: " + res2 + "<br>")
+document.write("Now the value of a is: " + a + "<br>" + "<br>")
+res3 = --a
+document.write("The value of --a is: " + res3 + "<br>")
+document.write("Now the value of a is: " + a + "<br>" + "<br>")
+res4 = a--
+document.write("The value of a-- is: " + res4 + "<br>")
+document.write("Now the value of a is: " + a + "<br>" + "<br>")
 // 14
-// a = 2
-// b = 1
-// final_result = --a - --b + ++b + b--
-// //              1  -  0  +  1  +  1
-
-// a = 1
-// b = 1
-// document.write("a is: " + a + "<br>")
-// document.write("b is: " + b + "<br>")
-// document.write("result is: " + final_result)
+a = 2
+b = 1
+final_result = --a - --b + ++b + b--
+//              1  -  0  +  1  +  1 
+a = 1
+b = 1
+document.write("a is: " + a + "<br>")
+document.write("b is: " + b + "<br>")
+document.write("final result after --a, - --b, + ++b, + b-- is: " + final_result)
 
 // 15
-// let heading = "<h1> Lifetime supply calculator </h1>"
-// let fav_snack = "Peanut pik Biscuits"
-// let currentAge = 20
-// let maxAge = 80
-// let amount = 10
-// let totalRs = (80 - 20) * 10 
-// document.write(heading + "<br>" + "<br>")
-// document.write("Favourite snack : " + fav_snack + "<br>")
-// document.write("Current age : " + currentAge + "<br>")
-// document.write("Estimated Maximum age : " + maxAge + "<br>")
-// document.write("Amount of snacks per day : " + amount + "<br>" + "<br>")
-// document.write("You will need " + totalRs + " Rs for " + fav_snack + " to last you until ripe old age of " + maxAge)
+let heading = "<h1> Lifetime supply calculator </h1>"
+let fav_snack = "Peanut pik Biscuits"
+let currentAge = 20
+let maxAge = 80
+let amount = 10
+let totalRs = (80 - 20) * 10 
+document.write(heading + "<br>" + "<br>")
+document.write("Favourite snack : " + fav_snack + "<br>")
+document.write("Current age : " + currentAge + "<br>")
+document.write("Estimated Maximum age : " + maxAge + "<br>")
+document.write("Amount of snacks per day : " + amount + "<br>" + "<br>")
+document.write("You will need " + totalRs + " Rs for " + fav_snack + " to last you until ripe old age of " + maxAge)*/
